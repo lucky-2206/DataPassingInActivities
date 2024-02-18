66b2c09bcb4d2933e16138f97b31d7fa50f81ab4
lucky_2206 <vaibhavjainlucky98@gmail.com>
2024-02-18 12:32:32 +0530

    Using view binding

--- app/src/main/java/com/example/learnandroid2/MainActivity.kt
+++ app/src/main/java/com/example/learnandroid2/MainActivity.kt
@@ -5,18 +5,24 @@ import android.os.Bundle
 import android.widget.EditText
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.learnandroid2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var forget_pass_button:TextView;
     lateinit var emailId :EditText
+    lateinit var binding :ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        forget_pass_button = findViewById(R.id.forgetPass)
-        emailId = findViewById(R.id.editTextTextEmailAddress)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        forget_pass_button = binding.forgetPass
+        emailId = binding.editTextTextEmailAddress
+
         forget_pass_button.setOnClickListener {
 
             val str = emailId.text.toString()
